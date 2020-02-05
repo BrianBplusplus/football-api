670d27bda547f9da0d287fa62b5a2bd54f8ec3a3
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-02-05 17:39:38 +0100

    implemented functionality to add teams

--- team/router.js
+++ team/router.js
@@ -8,4 +8,15 @@ router.get("/teams", (req, res, next) => {
   Team.findAll().then(teams => res.json(teams));
 });
 
+router.post("/teams", (req, res, next) => {
+  console.log("REQUEST BODY TO CREATE TEAM", req.body);
+  const { teamName } = req.body;
+  Team.create({ name: teamName })
+    .then(team => {
+      console.log("Created the team!");
+      res.json(team);
+    })
+    .catch(error => next(error));
+});
+
 module.exports = router;
